c07a31f2f385c90fca467de8ff0e7c9dab022158
Leonel Parrales <leonelparrales22@gmail.com>
2025-04-27 00:35:00 -0500

    Se modifica el modelo de la clase Vehiculo

--- src/app/modules/vehiculo/models/vehiculo.ts
+++ src/app/modules/vehiculo/models/vehiculo.ts
@@ -1,3 +1,10 @@
 export class Vehiculo {
-    
+  id?: number;
+  marcar?: string;
+  linea?: string;
+  referencia?: string;
+  modelo?: number;
+  kilometraje?: number;
+  color?: string;
+  imagen?: string;
 }
